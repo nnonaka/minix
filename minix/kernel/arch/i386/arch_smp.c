37a084938414d34c73dd2fe1a51216c04ff51f15
Naomichi Nonaka <nao.nonaka@gmail.com>
2021-04-11 19:44:14 +0900

    Revert "Fix sysenter and syscall features."
    This reverts commit a6a742a700dd2599dde29e4213e41344c5867a9c.

--- minix/kernel/arch/i386/arch_smp.c
+++ minix/kernel/arch/i386/arch_smp.c
@@ -218,9 +218,6 @@ static void ap_finish_booting(void)
 	/* inform the world of our presence. */
 	ap_cpu_ready = cpu;
 
-	/* Set up sysenter/syscall. */
-	setup_sysenter_syscall();
-
 	/*
 	 * Finish processor initialisation.  CPUs must be excluded from running.
 	 * lapic timer calibration locks and unlocks the BKL because of the
@@ -301,7 +298,6 @@ void smp_init (void)
 	ioapic_enabled = 0;
 
 	tss_init_all();
-	setup_sysenter_syscall();
 
 	/* 
 	 * we still run on the boot stack and we cannot use cpuid as its value
