318831d6d7cde3488a24998ea3806abb92306d9b
Naomichi Nonaka <nao.nonaka@gmail.com>
2023-02-19 10:38:44 +0900

    Add grub

---
+++ external/gpl3/grub/fetch.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+# Make sure we're in our directory (i.e., where this shell script is)
+echo $0
+cd `dirname $0`
+
+# Configure fetch method
+URL="https://ftp.gnu.org/gnu/grub/grub-2.06.tar.gz"
+FETCH=ftp
+which curl >/dev/null
+if [ $? -eq 0 ]; then
+	FETCH="curl -O -f"
+fi
+
+# Fetch sources if not available
+if [ ! -d dist ];
+then
+	if [ ! -f grub-2.06.tar.gz ]; then
+		$FETCH $URL
+		if [ $? -ne 0 ]; then
+			$FETCH $BACKUP_URL
+		fi
+	fi
+
+	tar -oxjf grub-2.06.tar.gz && \
+	mv grub-2.06 dist
+fi
+
